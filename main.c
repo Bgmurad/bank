c37ff90cad211b8f86a71d3b44aa636cbb7a2d9b
Bgmurad <bg.murad@gmail.com>
2015-01-08 21:34:38 +0300

    21:34
    first strings

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include "kernel/zaptel.h"
+#include <stdio.h>
+#include <linux/types.h>
+
+void dump_bits(unsigned char *outbuf, int len)
+{
+ int x,i;
+ for (x=0;x<len;x++) {
+  for (i=0;i<8;i++) {
+   if (outbuf[x] & (1 << (7-i)))
+    printf("1");
+   else
+    printf("0");
+  }
+ }
+ printf("\n");
+}
